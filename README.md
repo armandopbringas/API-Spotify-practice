122a1277e3fe92b95f5940f5b1b4355fd0e1e3d4
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2021-02-26 17:26:53 -0600

    API start

---
+++ README.md
@@ -0,0 +1,7 @@
+## Rules to live by
+
+* Coverage > 80%
+* No console.log in commits
+* `map`, `forEach`, `filter` first rather than for loops
+* Split your logic
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const lasDeLosAngelesParaLlorar = require("./fixtures/tracks-angeles.json");
+const elCorazonDeLasDeLosAngelesChidas = require("./fixtures/audio-features-angeles.json");
+
+//console.log(lasDeLosAngelesParaLlorar);
+//console.log(elCorazonDeLasDeLosAngelesChidas);
+
+// const tracksResponse = { tracks: [] };
+// const tracks = tracksResponse.tracks.map(track => track);
+
+// const tracksIds = tracks.map(({ id }) => id );
+
+
+// const audioFeatures = tracksIds.map(getAudioFeatures);
+
+// const energyAverage = (totalEnergy / tracksIds.length) || 0;
+
+
+
+
